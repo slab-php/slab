59e20b6c3a7fba46de49ef3c4bdfb8203150b9f3
Ben Scott <ben@belfryimages.com.au>
2013-02-07 21:37:54 +1000

    fix #3 example stub app

--- examples/stub/app/config.php
+++ examples/stub/app/config.php
@@ -2,7 +2,7 @@
 
 // Application
 Config::set('app.default_route', '/pages/index');
-Config::set('app.default_components', array('db', 'cookie', 'session', 'file', 'image'));
+Config::set('app.default_components', array(/*'db', */'cookie', 'session', 'file', 'image'));
 Config::set('app.default_helpers', array('html', 'number'));
 Config::set('app.load_model_schemas', true);
 Config::set('app.url_rewriting', true);
@@ -17,17 +17,26 @@ if ($_SERVER['SERVER_NAME'] == 'localhost') {
 	Config::set('db.driver', 'mysql');
 	Config::set('db.host', 'localhost');
 	Config::set('db.login', 'root');
-	Config::set('db.password', 'password');
-	Config::set('db.database', 'DATABASE_NAME');
+	Config::set('db.password', '');
+	Config::set('db.database', '');
+	Config::set('db.tablePrefix', '');
+} else if ($_SERVER['SERVER_NAME'] == 'staging.server.com') {
+	// staging server settings
+	Config::set('db.driver', 'mysql');
+	Config::set('db.host', '');
+	Config::set('db.port', '');
+	Config::set('db.login', '');
+	Config::set('db.password', '');
+	Config::set('db.database', '');
 	Config::set('db.tablePrefix', '');
 } else {
 	// live settings
 	Config::set('db.driver', 'mysql');
-	Config::set('db.host', 'mysql.server');
-	Config::set('db.port', '3306');
-	Config::set('db.login', 'LIVE_USERNAME');
-	Config::set('db.password', 'LIVE_PASSWORD');
-	Config::set('db.database', 'LIVE_DATABASE_NAME');
+	Config::set('db.host', '');
+	Config::set('db.port', '');
+	Config::set('db.login', '');
+	Config::set('db.password', '');
+	Config::set('db.database', '');
 	Config::set('db.tablePrefix', '');
 }
 
---
+++ examples/stub/app/views/layouts/default.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title><?php e($pageTitle); ?></title>
+</head>
+<body>
+
+	<h1>stub/app/views/layouts/default.php</h1>
+
+	<p>-- content starts --</p>
+	<?php e($pageContent); ?>
+	<p>-- content ends --</p>
+
+</body>
+</html>
