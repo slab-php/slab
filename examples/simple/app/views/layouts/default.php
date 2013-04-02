b2d85893c1f0a59388f90fc021a5f7e66bda67c1
Ben Scott <ben@belfryimages.com.au>
2013-04-02 15:46:03 +1000

    #32 fix simple example

--- examples/simple/app/views/layouts/default.php
+++ examples/simple/app/views/layouts/default.php
@@ -3,17 +3,20 @@
 	<title><?php e($pageTitle); ?></title>
 </head>
 <body>
-<h1>Default layout</h1>
+	<h1>Default layout</h1>
 
-<ul>
-	<li><a href="<?php e($html->url('/test')); ?>">Index</a></li>
-	<li><a href="<?php e($html->url('/test/hello_world')); ?>">Hello world</a></li>
-	<li><a href="<?php e($html->url('/test/hello_world_via_get')); ?>">Hello world via GET</a></li>
-	<li><a href="<?php e($html->url('/test/test_dispatch')); ?>">Test dispatch</a></li>
-</ul>
+	<ul>
+		<li><a href="<?php e($html->url('/test')); ?>">Index</a></li>
+		<li><a href="<?php e($html->url('/test/hello_world')); ?>">Hello world</a></li>
+		<li><a href="<?php e($html->url('/test/hello_world_via_get')); ?>">Hello world via GET</a></li>
+		<li><a href="<?php e($html->url('/test/test_dispatch')); ?>">Test dispatch</a></li>
+	</ul>
 
-<p>-- content starts --</p>
-<?php e($pageContent); ?>
-<p>-- content ends --</p>
+	<p>-- content starts --</p>
+	<?php e($pageContent); ?>
+	<p>-- content ends --</p>
+
+	<?php e($dispatcher->pageLogger->to_table()); ?>
+
 </body>
 </html>
--- examples/simple/slab.php
+++ examples/simple/slab.php
@@ -3,14 +3,27 @@ error_reporting(E_ALL);
 
 define('SLAB_ROOT', __FILE__);
 define('SLAB_APP', dirname(SLAB_ROOT).'/app');
-define('SLAB_LIB', dirname(SLAB_ROOT).'/../../lib');
+// This allows having a development path for the slab library:
+if ($_SERVER['SERVER_NAME'] == 'localhost') {
+	define('SLAB_LIB', dirname(SLAB_ROOT).'/../../lib');
+} else {
+	define('SLAB_LIB', dirname(SLAB_ROOT).'/lib');
+}
 
 require_once(SLAB_LIB.'/bootstrap.php');
 require_once(SLAB_APP.'/config.php');
 
-$dispatcher = new Dispatcher($config);
+$pageLogger = new PageLogger();
+$pageLogger->log('slab', 'slab', 'init');
+$dispatcher = new Dispatcher($config, $pageLogger);
 
-$actionResult = $dispatcher->dispatch();
-$actionResult->render();
+try {
+	$actionResult = $dispatcher->dispatch();
+	$actionResult->render();	
+} catch (Exception $ex) {
+	$dispatcher->handle_exception($ex);
+}
+
 $dispatcher->shutdown();
+$pageLogger->log('slab', 'slab', 'end');
 ?>
